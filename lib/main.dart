e0e205d291e1bb199baeb4c9e3d6b3eb98afa285
Salih DAMAR <47279445+salihdamar@users.noreply.github.com>
2023-02-25 14:22:56 +0300

    tek sayfa uygulama

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(
+    BenimUyg(),
+  );
+}
+
+class BenimUyg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.brown[200],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundImage: AssetImage("assets/images/kahve.jpg"),
+                ),
+                Text(
+                  "Flutter Kahvecisi",
+                  style: TextStyle(
+                    fontFamily: "Satisfy",
+                    fontSize: 45,
+                    color: Colors.brown[900],
+                  ),
+                ),
+                Text(
+                  "BİR FİNCAN UZAĞINIZDA",
+                  style: GoogleFonts.dancingScript(
+                    fontSize: 26,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  child: Divider(
+                    height: 30,
+                    color: Colors.brown,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 45.0,
+                  ),
+                  color: Colors.brown,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "bilprog@gmail.com",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 45.0,
+                  ),
+                  color: Colors.brown,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "0555 555 55 55",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 45.0,
+                  ),
+                  color: Colors.brown,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.telegram,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      "Telegram",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
